7af52522227f60ab7e995436c35fe98a0555108e
Lucas Diniz <lucas_diniz_costa@hotmail.com>
2017-08-10 22:27:30 -0300

    Adiciona uma imagem e alinha

--- src/aula02exm01/Janela.java
+++ src/aula02exm01/Janela.java
@@ -2,8 +2,11 @@ package aula02exm01;
 
 import java.awt.FlowLayout;
 import java.awt.HeadlessException;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
 public class Janela extends JFrame{
     private final JLabel etiqueta;              //Só será instanciado uma vez
@@ -11,8 +14,12 @@ public class Janela extends JFrame{
 
     public Janela() throws HeadlessException {  //Exceção se acionar o programa em um aparelho sem tela
         super("Exemplo janela");    //Chamando o construtor da super classe, passando o titulo da janela
-        setLayout(new FlowLayout());
-        etiqueta = new JLabel("Universidade Federal de Juiz de Fora!");      //Criando uma etiqueta para inserir na janela                                  
+        setLayout(new FlowLayout());                                         //Atribuindo um layout
+        Icon iconeUFJF = new ImageIcon("Resources/UFJF.jpg");
+        etiqueta = new JLabel("Universidade Federal de Juiz de Fora!");      //Criando uma etiqueta para inserir na janela
+        etiqueta.setIcon(iconeUFJF);
+        etiqueta.setHorizontalTextPosition(SwingConstants.CENTER);
+        etiqueta.setVerticalTextPosition(SwingConstants.BOTTOM);
         etiqueta2 = new JLabel("Departamento de Ciência da Computação");     //Criando uma etiqueta para inserir na janela
         add(etiqueta);                                       //adicionando na janela
         add(etiqueta2);                                      //adicionando na janela
